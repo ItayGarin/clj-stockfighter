240d1379e67649f1d855cc6cc63ed1a4a4cb81d8
thifi <thifixp@gmail.com>
2015-12-22 07:56:57 +0200

    Add websocket client API dependencies

--- project.clj
+++ project.clj
@@ -5,4 +5,6 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.7.0"]
                  [clj-http "2.0.0"]
-                 [cheshire "5.5.0"]])
+                 [cheshire "5.5.0"]
+                 [aleph "0.4.1-beta3"]
+                 [manifold "0.1.2-alpha1"]])
